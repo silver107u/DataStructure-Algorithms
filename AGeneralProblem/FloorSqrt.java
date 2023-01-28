3343b22c61a7fc2bf13d125e19754e546593abda
Ayush Garg <ayushgarg9752@gmail.com>
2023-01-28 08:34:11 +0530

    Adding General Problem floor squre root and squre root

---
+++ AGeneralProblem/FloorSqrt.java
@@ -0,0 +1,16 @@
+// W.A.P to find the floor root of a number. 
+
+package session_1;
+// Time complexity--> root of N
+
+
+public class FlootSqrt {
+	public static void main(String[] args) {
+		int n=170;
+		int i=1;
+		for(;i*i<=n;i++) {
+			
+		}
+		System.out.println("floor squre root is "+(i-1));
+	}
+}
